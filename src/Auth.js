cfd5520181ba85406954823b34e3f858cd54f0d6
Mike A <mike@ackermanpla.net>
2021-02-18 11:15:41 -0500

    start auth changes

--- src/Auth.js
+++ src/Auth.js
@@ -1,12 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import firebase from './firebase';
 
 export const AuthContext = React.createContext();
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
 
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser ] = useState(null);
     const [pending, setPending ] = useState(true);
 
+    function logout() {
+        setCurrentUser();
+        return firebase.auth.signOut();
+    }
+
+    function updateEmail(email) {
+        //console.log('got into update email');
+        try {
+            return currentUser.updateEmail(email);
+        } catch(err) {
+            return (`error updating email: ${err}`, null);
+        }
+        
+    }    
+
+    function updatePassword(password) {
+        return currentUser.updatePassword(password)
+    }
+    
     useEffect(() => {
         firebase.auth().onAuthStateChanged((user) =>{
             setCurrentUser(user)
